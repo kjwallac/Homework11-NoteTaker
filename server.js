13259cb3e2a850650c14d4700741ddb5f02e73df
kjwallac <kristinajwallace@gmail.com>
2021-03-12 22:32:27 -0800

    use static for public directory, updated routes

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 require('./routes/apiRoutes')(app);
-require('./routes/htmlRoutes')(app);
+// require('./routes/htmlRoutes')(app);
 
 app.listen(PORT, () => {
   console.log(`App listening on http://localhost:${PORT}`);
